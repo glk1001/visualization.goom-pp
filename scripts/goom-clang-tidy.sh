c7a569a19fd1f959f7e014fa4609b26523d84eb6
glk1001 <gregg.kay@gmail.com>
2024-08-08 12:26:09 +1000

    TEMP: clang-tidy temp script hack.

--- scripts/goom-clang-tidy.sh
+++ scripts/goom-clang-tidy.sh
@@ -15,7 +15,8 @@ else
 fi
 declare -r GOOM_MAIN_SRCE_DIR="${GOOM_MAIN_ROOT_DIR}/src"
 declare -r GOOM_LIB_DIR="${GOOM_MAIN_ROOT_DIR}/depends/goom-libs/src"
-declare -r CLANG_TIDY_LOG="/tmp/goom-clang-tidy.log"
+#declare -r CLANG_TIDY_LOG="/tmp/goom-clang-tidy.log"
+declare -r CLANG_TIDY_LOG="${HOME}/Prj/workdir/goom-clang-tidy.log"
 
 if [[ ! -d "${BUILD_DIRNAME}" ]]; then
   >&2 echo "ERROR: Could not find build dir: \"${BUILD_DIRNAME}\"."
@@ -41,7 +42,10 @@ fi
 
 SECONDS=0
 
-"${RUN_CLANG_TIDY}" -j $(getconf _NPROCESSORS_ONLN) -header-filter='^((?!catch2).)*$' \
+#declare -r NUM_PROCS=$(getconf _NPROCESSORS_ONLN)
+declare -r NUM_PROCS=2
+
+"${RUN_CLANG_TIDY}" -fix -export-fixes=${GOOM_MAIN_SRCE_DIR}/fixes.yaml -j ${NUM_PROCS} -header-filter='^((?!catch2).)*$' \
                     -extra-arg=-Wno-unknown-warning-option -p "${BUILD_DIRNAME}" \
                     "${GOOM_MAIN_SRCE_DIR}" "${GOOM_LIB_DIR}" |& tee "${CLANG_TIDY_LOG}"
 if [[ $? != 0 ]]; then
