8fa75fb48a292a0d8a8adaba0ae37f35c7f4b435
glk1001 <gregg.kay@gmail.com>
2024-09-16 20:19:17 +1000

    Added rotation to vortex.

--- depends/goom-libs/src/goom/src/filter_fx/gpu_filter_effects/vortex.cpp
+++ depends/goom-libs/src/goom/src/filter_fx/gpu_filter_effects/vortex.cpp
@@ -15,7 +15,7 @@ using UTILS::MATH::NumberRange;
 
 static constexpr auto AMPLITUDE_RANGE       = NumberRange{1.0F, 3.0F};
 static constexpr auto BASE_RANGE            = NumberRange{0.00F, 0.03F};
-static constexpr auto CYCLE_FREQUENCY_RANGE = NumberRange{1.0F, 3.0F};
+static constexpr auto CYCLE_FREQUENCY_RANGE = NumberRange{0.1F, 0.5F};
 static constexpr auto FREQUENCY_RANGE       = NumberRange{0.005F, 0.030F};
 static constexpr auto POSITION_FACTOR_RANGE = NumberRange{0.1F, 0.2F};
 static constexpr auto R_FACTOR_RANGE        = NumberRange{5.0F, 15.0F};
@@ -31,6 +31,7 @@ static constexpr auto VIEWPORT_BOUNDS = RandomViewport::Bounds{
 static constexpr auto PROB_XY_AMPLITUDES_EQUAL        = 0.98F;
 static constexpr auto PROB_XY_BASES_EQUAL             = 0.98F;
 static constexpr auto PROB_XY_CYCLE_FREQUENCIES_EQUAL = 0.98F;
+static constexpr auto PROB_NEGATIVE_SPIN_SIGN         = 0.50F;
 static constexpr auto PROB_NO_VIEWPORT                = 0.5F;
 
 Vortex::Vortex(const GoomRand& goomRand) noexcept
@@ -65,6 +66,8 @@ auto Vortex::GetRandomParams() const noexcept -> GpuParams
   const auto positionFactor = m_goomRand->GetRandInRange<POSITION_FACTOR_RANGE>();
   const auto rFactor        = m_goomRand->GetRandInRange<R_FACTOR_RANGE>();
 
+  const auto vortexSpinSign = m_goomRand->ProbabilityOf(PROB_NEGATIVE_SPIN_SIGN) ? -1.0F : +1.0F;
+
   return GpuParams{
       viewport,
       {.x = xAmplitude, .y = yAmplitude},
@@ -73,6 +76,7 @@ auto Vortex::GetRandomParams() const noexcept -> GpuParams
       frequencyFactor,
       positionFactor,
       rFactor,
+      vortexSpinSign,
   };
 }
 
@@ -87,14 +91,16 @@ Vortex::GpuParams::GpuParams(const Viewport& viewport,
                              const FrequencyFactor& cycleFrequency,
                              const float frequencyFactor,
                              const float positionFactor,
-                             const float rFactor) noexcept
+                             const float rFactor,
+                             const float vortexSpinSign) noexcept
   : m_viewport{viewport},
     m_amplitude{amplitude},
     m_filterBase{filterBase},
     m_cycleFrequency{cycleFrequency},
     m_frequencyFactor{frequencyFactor},
     m_positionFactor{positionFactor},
-    m_rFactor{rFactor}
+    m_rFactor{rFactor},
+    m_vortexSpinSign{vortexSpinSign}
 {
 }
 
@@ -112,6 +118,7 @@ auto Vortex::GpuParams::OutputGpuParams(const FilterTimingInfo& filterTimingInfo
   setterFuncs.setFloat("u_vortexFreq", m_frequencyFactor);
   setterFuncs.setFloat("u_vortexPositionFactor", m_positionFactor);
   setterFuncs.setFloat("u_vortexRFactor", m_rFactor);
+  setterFuncs.setFloat("u_vortexSpinSign", m_vortexSpinSign);
 }
 
 } // namespace GOOM::FILTER_FX::GPU_FILTER_EFFECTS
